60cb1113b19a50cdbfcea93345b9000963465235
Viktoria-create <viktoriakoshelnik@yandex.ru>
2023-04-05 23:21:37 +0300

    30

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -82,11 +82,10 @@ SQLITE = {
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', 'postgres'),
-        'USER': os.getenv('POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+        'NAME': os.getenv('DB_NAME', 'foodgram'),
+        'USER': os.getenv('POSTGRES_USER', 'foodgram_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'foodgram_password'),
         'HOST': os.getenv('DB_HOST', '127.0.0.1'),
-        'PORT': os.getenv('DB_PORT', '5432'),
         'OPTIONS': {'options': '-c timezone=UTC'},
     }
 }

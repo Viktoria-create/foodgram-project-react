6f859ce09f27a2848082c3c8d041e1a05cf9e6bd
Viktoria-create <viktoriakoshelnik@yandex.ru>
2023-04-06 14:35:17 +0300

    30

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -14,7 +14,7 @@ if not SECRET_KEY:
     SECRET_KEY = ''.join(secrets.choice(string.ascii_letters)
                          for _ in range(50))
 
-DEBUG = int(os.environ.get('DEBUG', default=0))
+DEBUG = True  # int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS',
                                default='51.250.20.77 localhost').split(" ")

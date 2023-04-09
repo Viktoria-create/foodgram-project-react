1cce729b45538a6c7cb672ed4446440997b36d5c
Viktoria-create <viktoriakoshelnik@yandex.ru>
2023-04-09 17:35:55 +0300

    30

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -164,18 +164,7 @@ class RecipeCreateUpdateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fields = (
-            "id",
-            "tags",
-            "author",
-            "ingredients",
-            "is_favorited",
-            "is_in_shopping_cart",
-            "name",
-            "image",
-            "text",
-            "cooking_time",
-        )
+        exclude = ('pub_date',)
 
 
 class ShortRecipeSerializer(serializers.ModelSerializer):
